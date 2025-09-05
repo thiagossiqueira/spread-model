2be48c886c04109f82ffdf16c617c543fe27cd30
tsiqueira4 <thiagossiqueira@gmail.com>
2025-09-04 23:34:22 -0300

    Refatoração de filters.py: adicionado .copy() para prevenir side-effects e garantir segurança nas operações de filtragem

--- src/utils/filters.py
+++ src/utils/filters.py
@@ -1,7 +1,6 @@
 # utils/filters.py
 import pandas as pd
 
-
 def filter_corporate_universe(df: pd.DataFrame, inflation_linked: str = "N") -> pd.DataFrame:
     df = df.copy()
     print(f"🔍 Inicial: {len(df)} linhas")
@@ -15,17 +14,16 @@ def filter_corporate_universe(df: pd.DataFrame, inflation_linked: str = "N") -> 
     df = df[df['CPN_TYP'].isin(['FIXED'])]
     print(f"➡ Após filtrar CPN_TYP='FIXED': {len(df)}")
 
-    # df = df[df['MTY_TYP'].isin(['AT MATURITY'])]
-    # print(f"➡ Após filtrar MTY_TYP='AT MATURITY': {len(df)}")
-
     df = df[df['CRNCY'].isin(['BRL'])]
     print(f"➡ Após filtrar CRNCY='BRL': {len(df)}")
 
     print("🧪 Valores únicos em INFLATION_LINKED_INDICATOR:", df["INFLATION_LINKED_INDICATOR"].unique())
+    df = df.copy()
     df["INFLATION_LINKED_INDICATOR"] = df["INFLATION_LINKED_INDICATOR"].astype(str).str.strip().str.upper()
     df = df[df["INFLATION_LINKED_INDICATOR"].isin([inflation_linked])]
     print(f"➡ Após filtrar INFLATION_LINKED_INDICATOR={inflation_linked}: {len(df)}")
 
+    df = df.copy()
     df['TOT_DEBT_TO_EBITDA'] = pd.to_numeric(df['TOT_DEBT_TO_EBITDA'], errors='coerce')
     print(f"➡ Após conversão de TOT_DEBT_TO_EBITDA (com NaN): {df['TOT_DEBT_TO_EBITDA'].isna().sum()} NaNs")
 
@@ -36,10 +34,6 @@ def filter_corporate_universe(df: pd.DataFrame, inflation_linked: str = "N") -> 
     df["id"] = df["id"].astype(str).str.strip()
 
     return df
-
-
-
-# utils/filters.py
 
 def anomaly_filtering_results(df: pd.DataFrame) -> pd.DataFrame:
     """
