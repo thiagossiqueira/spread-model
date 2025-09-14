f302ffa516d0b26172565b90576d57faf021ed6d
tsiqueira4 <thiagossiqueira@gmail.com>
2025-09-14 09:32:46 -0300

    feat(wsgi): adiciona logging simultâneo no console e arquivo wsgi_log.txt

--- wsgi.py
+++ wsgi.py
@@ -1,6 +1,24 @@
 import sys
 import os
 
+# Redireciona print() para console + arquivo de log
+class Logger:
+    def __init__(self, filename="wsgi_log.txt"):
+        self.terminal = sys.stdout
+        log_path = os.path.join(os.path.dirname(__file__), filename)
+        self.log = open(log_path, "a", encoding="utf-8")
+
+    def write(self, message):
+        self.terminal.write(message)
+        self.log.write(message)
+
+    def flush(self):
+        self.terminal.flush()
+        self.log.flush()
+
+# Ativa o logger (apenas uma vez no deploy)
+sys.stdout = sys.stderr = Logger()
+
 # Caminho absoluto para o diretório do projeto
 project_home = '/home/tsiqueira4/spread-model'
 
