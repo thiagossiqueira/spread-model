d2ef6758aae595570012c0df29f746269ef44066
tsiqueira4 <thiagossiqueira@gmail.com>
2025-09-14 22:19:05 -0300

    Summary IPCA investigation

--- app.py
+++ app.py
@@ -45,9 +45,9 @@ def spread_table(prefixo):
 @app.route("/summary/<prefixo>")
 def summary(prefixo):
     if prefixo == "di":
-        return send_file("templates/summary_DI_table.html")
+        return send_file("templates/di_summary_table.html")
     elif prefixo == "ipca":
-        return send_file("templates/summary_IPCA_table.html")
+        return send_file("templates/ipca_summary_table.html")
     else:
         return "Tipo inválido", 400
 
